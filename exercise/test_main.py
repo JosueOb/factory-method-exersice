08346f5e1d88c2d852447a5c215f8073d3c718d2
raizo2000 <arguanoluisa1@espe.edu.ec>
2022-06-23 16:22:16 -0500

    encode-decode

---
+++ exercise/test_main.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+from main import InvoiceFactory, ShippingLabelFactory, Document
+
+
+def test_invoice_factory():
+    
+    assert InvoiceFactory().create_document('INVOICE TEST') == Document(content='This is a single string ==> INVOICE TEST')
+
+def test_shipping_label_factory():
+    assert ShippingLabelFactory().create_document('SHIPPING LABEL TEST') == Document(content='This is a binary data ==> SHIPPING LABEL TEST')
+
+def test_document():
+    assert Document(content='This is a single string ==> INVOICE TEST') == Document(content='This is a single string ==> INVOICE TEST')
+    assert Document(content='This is a binary data ==> SHIPPING LABEL TEST') == Document(content='This is a binary data ==> SHIPPING LABEL TEST')
--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 # #
 # document = PyPDF2.PdfReader("docs/label-test.pdf").getDocumentInfo()
 # print(type(document))
+import base64
+from typing_extensions import dataclass_transform
 import pydantic
-
-import io
-
 
 # Read the pdf file and convert to bytes
 class Document(pydantic.BaseModel):
@@ -13,6 +12,20 @@ class Document(pydantic.BaseModel):
     title: str = "This is a PDF file"
 
 
-with open("docs/label-test.pdf", 'r') as pdf:
-    file = io.TextIOWrapper(pdf)
-    print(type(file))
+class Operation:
+    def convert_to_bytes(self, label_route: str) -> None:
+        with open(label_route, 'rb') as file:
+            encode_pdf = base64.b64encode(file.read())
+
+        return encode_pdf
+
+    def convert_to_pdf(self, bytes_data: bytes) -> None:
+        pdf_data = bytes_data.decode('utf-8')
+        print(type(pdf_data))
+
+if __name__ == "__main__":
+    label_route = "./docs/label.pdf"
+    operation = Operation()
+    bytes_data = operation.convert_to_bytes(label_route)
+    print(type(bytes_data))
+    operation.convert_to_pdf(bytes_data)
