19e93086717ae66448ad7b8ac2d4eeb85fcbbd46
JosueOb <josuericardocando@hotmail.com>
2022-06-23 14:56:21 -0500

    Add PDF file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-"""
-Factory Method Pattern design
-"""
+# import PyPDF2
+# #
+# document = PyPDF2.PdfReader("docs/label-test.pdf").getDocumentInfo()
+# print(type(document))
+import pydantic
+
+import io
+
+
+# Read the pdf file and convert to bytes
+class Document(pydantic.BaseModel):
+    content: bytes
+    title: str = "This is a PDF file"
+
+
+with open("docs/label-test.pdf", 'r') as pdf:
+    file = io.TextIOWrapper(pdf)
+    print(type(file))
